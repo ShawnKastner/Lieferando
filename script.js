cba358088cce0ef0ab35868771b53d30d494e863
Shawn Kastner <Shawnkastner@hotmail.de>
2022-11-21 17:19:20 +0100

    add renderBasket(); updateShoppingBasket()

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-let foods = []
-let prices = []; let shoppingBasket = [];
+let basketFood = [];
+let basketPrice = [];
+let basketAmount = [];
+let shoppingBasket = [];
 
+function renderBasket() {
+    let basket = document.getElementById('shoppingCart');
+    basket.innerHTML = '';
 
-function addToBasket(name, price) {
-    shoppingBasket.push(name); prices.push(price);
+    for (let i = 0; i < basketFood.length; i++) {
+        
+        basket.innerHTML += basketTemplate(basketFood, basketPrice, basketAmount);
+    }
+    updateShoppingBasket()
+}
+
+function basketTemplate(basketFood, basketPrice, basketAmount) {
+    return `<div class="listItem">
+           <b>${basketAmount}x ${basketFood}:</b> 
+           <div id="newPrice">${basketPrice}€</div>
+        </div>`;
+}
+
+function addToBasket(food, price) {
+    let index = basketFood.indexOf(food);
+    if (index == -1) {
+        basketFood.push(food);
+        basketPrice.push(price);
+        basketAmount.push(1)
+    } else {
+        ;
+        basketAmount[index]++;
+    }
+    renderBasket();
 }
 
 function updateShoppingBasket() {
     let sum = 0;
 
+    for (let i = 0; i < basketPrice.length; i++) {
+        sum += basketPrice[i] * basketAmount[i];
 
-    for (let i = 0; i < prices.length; i++) {
-        sum += prices[i]
-
-        document.getElementById().innerHTML = sum;
+        document.getElementById('newPrice').innerHTML = `${sum.toFixed(2)}€;`
     }
 }
